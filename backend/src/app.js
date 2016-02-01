05c2a2dcaa053e764dd1a789b0dd2660aa958fae
Lonk <rinke2005@gmail.com>
2016-02-01 21:48:02 +0100

    Add thinky integration and first models

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import models from './models';
 
 const app = express();
+
+app.locals.models = models;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
