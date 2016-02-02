c8051a1ba75adb7ee25a90c75f8f8d3e243a6122
Lonk <rinke2005@gmail.com>
2016-02-02 22:01:31 +0100

    Change structure

---
+++ static/src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mdl-grid">
+        <div class="mdl-cell poow-cell mdl-cell--4-col">
+            <h3>Poow</h3>
+            Descriptif...
+        </div>
+        <div class="mdl-cell poow-cell mdl-cell--4-col">
+            <h3>News</h3>
+            Dernière actu...
+        </div>
+        <div class="mdl-cell poow-cell mdl-cell--4-col">
+            <h3>Classement</h3>
+            Tableau
+        </div>
+    </div>
+</template>
